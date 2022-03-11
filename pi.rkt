f98892e3dd23f37a5c6902d098f5d293cc259c69
Palash Bauri <palashbauri1@gmail.com>
2022-03-11 08:30:13 +0530

    code formatted with raco fmt

--- pi.rkt
+++ pi.rkt
@@ -1,8 +1,6 @@
 #lang racket
 
-(require math/number-theory
-         racket/flonum
-         math/bigfloat)
+(require math/bigfloat)
 
 ;==== Simple implementation ====
 ;(define con_x (/ 12 (expt 640320 (/ 3 2))))
@@ -16,7 +14,6 @@
 
 (bf-precision 5000)
 
-
 ;converting some 'constant' integers
 (define b12 (bf 12))
 (define b640320 (bf 640320))
@@ -27,21 +24,20 @@
 (define b545140134 (bf 545140134))
 (define b-640320 (bf -640320))
 
-
 ;first part of the Chudnovsky formula
 ;
 (define con_a (bf/ (bf 12) (bfexpt (bf 640320) (bf/ (bf 3) (bf 2)))))
-
-
-
 
 (define output (bf 0))
 (define max 10000)
 
 (for ([i (in-range max)])
   (define k (bf i))
-  (define y (bf/ (bf* (bffactorial (bigfloat->real (bf* b6 k))) (bf+ b13591409 (bf* b545140134 k)))
-       (bf* (bffactorial (bigfloat->real (bf* b3 k))) (bfexpt (bffactorial (bigfloat->real k)) b3) (bfexpt b-640320 (bf* b3 k )))))
+  (define y
+    (bf/ (bf* (bffactorial (bigfloat->real (bf* b6 k))) (bf+ b13591409 (bf* b545140134 k)))
+         (bf* (bffactorial (bigfloat->real (bf* b3 k)))
+              (bfexpt (bffactorial (bigfloat->real k)) b3)
+              (bfexpt b-640320 (bf* b3 k)))))
   (set! output (bf+ output y))
   ;(print output)
   )
@@ -52,5 +48,3 @@
 (define p (bf/ b1 a))
 
 (print (bigfloat->string ans))
-
-
